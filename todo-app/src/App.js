ac41b79ac4cf8ba4445108c3a711bade975aa425
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-05 14:20:34 +0100

    Config ES Lint and Prettier to working with React

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header    className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         To Do App
-        </p>
+        <p>To Do App </p>
         <a
           className="App-link"
           href="https://reactjs.org"
