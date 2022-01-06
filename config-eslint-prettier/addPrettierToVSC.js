4fc1b13f0b28294ce71e1f4a6e385169b1c22f26
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-06 11:33:58 +0100

    Add config file how add prettier to Visual Studio Code

---
+++ config-eslint-prettier/addPrettierToVSC.js
@@ -0,0 +1,33 @@
+//How can I add a prettier to Visual Studio Code ?
+/*
+1) Install Prettier-Code Formatter
+
+2) Settings -> Editor:Default Formatter -> Chose "Prettier - Code Formatter"
+
+3) Settings -> Arrow Parrens -> Chose to avoid
+
+4) Settings -> Prettier JSX -> Chose two Settings
+    1. Prettier Jsx Bracket Same Line
+    2. PrettierL Jsx Single Quote
+
+5) Settings -> Prettier Semi -> Chose this setting
+
+6) Settings -> Prettier Use Tabs -> Chose this setting
+
+7) Go to JSON Settings ->  
+    ...
+    "[html]": {
+        "editor.defaultFormatter": "vscode.html-language-features"
+    },
+        
+    "emmet.syntaxProfiles": {
+        "html": {
+        "inline_break": 2
+        }
+    },
+    "emmet.variables": {
+        "lang": "pl"
+    }
+    ...
+
+*/
