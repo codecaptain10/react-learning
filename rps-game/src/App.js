d64134faba65c58729a17d791a2a615b99175069
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-13 22:52:20 +0100

    Crete game structure and create a function to click

--- rps-game/src/App.js
+++ rps-game/src/App.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const [userChoice, setUserChoice] = useState(null);
+
+  const handleClick = value => {
+    setUserChoice(value);
+  };
+
+  return (
+    <div>
+      <h1>User choice is: {userChoice}</h1>
+      <h1>Computer choice is: </h1>
+      <button
+        onClick={() => {
+          handleClick("rock");
+        }}>
+        Rock
+      </button>
+      <button
+        onClick={() => {
+          handleClick("paper");
+        }}>
+        Paper
+      </button>
+      <button
+        onClick={() => {
+          handleClick("scissors");
+        }}>
+        Scissors
+      </button>
+    </div>
+  );
 };
 
 export default App;
