a69b0af391359f5b3093f40d7cf1a122ff35c302
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-14 02:54:04 +0100

    Create a computer choice and function to right working

--- rps-game/src/App.js
+++ rps-game/src/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+/* eslint-disable default-case */
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [userChoice, setUserChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
   const choices = ["rock", "paper", "scissors"];
 
   const handleClick = value => {
@@ -11,15 +13,41 @@ const App = () => {
   };
 
   const generateComputerChoice = () => {
-    const randomNumber = Math.random() * choices.length;
+    const randomNumber = Math.floor(Math.random() * choices.length);
     const randomChoice = choices[randomNumber];
     setComputerChoice(randomChoice);
+    checkResult();
+  };
+
+  useEffect(() => {
+    checkResult();
+  }, [computerChoice, userChoice]);
+
+  const checkResult = () => {
+    switch (userChoice + computerChoice) {
+      case "scissorspaper":
+      case "rockscissors":
+      case "paperrock":
+        setResult("YOU WIN! :)");
+
+        break;
+      case "paperscissors":
+      case "scissorsrock":
+      case "rockpaper":
+        setResult("YOU LOSE... :( ");
+        break;
+      case "paperpaper":
+      case "scissorsscissors":
+      case "rockrock":
+        setResult("DRAW :| ");
+        break;
+    }
   };
 
   return (
     <div>
       <h1>User choice is: {userChoice}</h1>
-      <h1>Computer choice is: </h1>
+      <h1>Computer choice is: {computerChoice}</h1>
       {choices.map((choice, index) => (
         <button
           key={index}
@@ -29,6 +57,7 @@ const App = () => {
           {choice}
         </button>
       ))}
+      <h1>{result}</h1>
     </div>
   );
 };
