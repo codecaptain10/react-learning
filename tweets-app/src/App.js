020cd08b6554271df897879ee5f9325622a75aee
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-17 13:47:01 +0100

    Give style for components and add css classes for Tweet and App

--- tweets-app/src/App.js
+++ tweets-app/src/App.js
@@ -3,7 +3,7 @@ import Tweet from "./components/Tweet";
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <h1>Hello React Tweets App!</h1>
       <Tweet />
     </div>
--- tweets-app/src/components/Tweet.js
+++ tweets-app/src/components/Tweet.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../App.css";
 
 function Tweet() {
   return (
-    <div>
+    <div className='tweet'>
       <h3>Name</h3>
       <p>Random tweet message</p>
       <h3>Number of likes</h3>
