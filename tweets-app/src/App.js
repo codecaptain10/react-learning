94372cba8f2e0f938ccc514b949b2388aa2c18a6
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-17 15:15:12 +0100

    Add all property to tweets and update message style

--- tweets-app/src/App.js
+++ tweets-app/src/App.js
@@ -8,10 +8,42 @@ function App() {
         <h1>Hello React Tweets App!</h1>
       </div>
       <div className='app'>
-        <Tweet name='Tony Stark' />
-        <Tweet name='Steve Rogers' />
-        <Tweet name='Scott Lang' />
-        <Tweet name='Stephen Strange' />
+        <Tweet
+          name='Tony Stark'
+          message='The true is ... I am Iron Man.'
+          likes={233 + " likes"}
+        />
+        <Tweet
+          name='Steve Rogers'
+          message='I can do this all day!'
+          likes={314 + " likes"}
+        />
+        <Tweet
+          name='Scott Lang'
+          message='You have been in space!?'
+          likes={814 + " likes"}
+        />
+        <Tweet
+          name='Stephen Strange'
+          message='Doctor. I am doctor Stephen Stragne.'
+          likes={107 + " likes"}
+        />
+        <Tweet
+          name='Thor'
+          message='I am Thor, the God of Thunder!'
+          likes={3011 + " likes"}
+        />
+        <Tweet
+          name='Clint Barton'
+          message='Did you think, I would miss?'
+          likes={442 + " likes"}
+        />
+        <Tweet name='Hulk' message='Smaaaash!!!' likes={4243 + " likes"} />
+        <Tweet
+          name='Thanos'
+          message='Perfect with balanc...this is how everything always have to should be.'
+          likes={112 + " likes"}
+        />
       </div>
     </div>
   );
--- tweets-app/src/components/Tweet.js
+++ tweets-app/src/components/Tweet.js
@@ -5,8 +5,8 @@ function Tweet(props) {
   return (
     <div className='tweet'>
       <h3>{props.name}</h3>
-      <p>Random tweet message</p>
-      <h3>Number of likes</h3>
+      <p>{props.message}</p>
+      <h3>{props.likes}</h3>
     </div>
   );
 }
