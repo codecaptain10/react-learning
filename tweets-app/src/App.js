d8331710556dc107e5f0f2cc58f1e8e20fbc7981
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-18 00:52:15 +0100

    Optimize the app with useState

--- tweets-app/src/App.js
+++ tweets-app/src/App.js
@@ -1,70 +1,80 @@
 import React from "react";
+import { useState } from "react";
 import Tweet from "./components/Tweet";
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      name: "Tony Stark",
+      message: "The true is ... I am Iron Man.",
+      likes: 9233 + " likes",
+    },
+    {
+      name: "Steve Rogers",
+      message: "I can do this all day!",
+      likes: 8314 + " likes",
+    },
+    {
+      name: "Scott Lang",
+      message: "You have been in space!?",
+      likes: 3814 + " likes",
+    },
+    {
+      name: "Stephen Strange",
+      message: "Doctor. I am doctor Stephen Stragne.",
+      likes: 1107 + " likes",
+    },
+    {
+      name: "Thor",
+      message: "I am Thor, the God of Thunder!",
+      likes: 3011 + " likes",
+    },
+    {
+      name: "Loki",
+      message: "Glorious purpose.",
+      likes: 5231 + " likes",
+    },
+    {
+      name: "Peter Parker",
+      message: "Hi! I am your friendly Spiderman.",
+      likes: 9201 + " likes",
+    },
+    {
+      name: "Kang",
+      message: "I am Kang, The Counquer.",
+      likes: 2231 + " likes",
+    },
+    {
+      name: "Shang Chi",
+      message: "I am a kung fu master!",
+      likes: 1231 + " likes",
+    },
+    {
+      name: "Ikars",
+      message: "I love you, Sersi.",
+      likes: 2912 + " likes",
+    },
+    {
+      name: "Thanos",
+      message: "Perfect with balance...it is how always should be.",
+      likes: 1731 + " likes",
+    },
+    {
+      name: "Hulk",
+      message: "Smaaash!",
+      likes: 1002 + " likes",
+    },
+  ]);
+
   return (
     <div className='container'>
       <div className='baner'>
         <h1>Hello React Tweets App!</h1>
       </div>
       <div className='app'>
-        <Tweet
-          name='Tony Stark'
-          message='The true is ... I am Iron Man.'
-          likes={233 + " likes"}
-        />
-        <Tweet
-          name='Steve Rogers'
-          message='I can do this all day!'
-          likes={314 + " likes"}
-        />
-        <Tweet
-          name='Scott Lang'
-          message='You have been in space!?'
-          likes={814 + " likes"}
-        />
-        <Tweet
-          name='Stephen Strange'
-          message='Doctor. I am doctor Stephen Stragne.'
-          likes={107 + " likes"}
-        />
-        <Tweet
-          name='Thor'
-          message='I am Thor, the God of Thunder!'
-          likes={3011 + " likes"}
-        />
-        <Tweet
-          name='Clint Barton'
-          message='Did you think, I would miss?'
-          likes={442 + " likes"}
-        />
-        <Tweet name='Hulk' message='Smaaaash!!!' likes={4243 + " likes"} />
-        <Tweet
-          name='Thanos'
-          message='Perfect with balance...this is how everything always have to should be.'
-          likes={112 + " likes"}
-        />
-
-        <Tweet
-          name='Loki'
-          message='Glorious purpose.'
-          likes={5231 + " likes"}
-        />
-        <Tweet
-          name='Nick Fury'
-          message='We need heroes.'
-          likes={231 + " likes"}
-        />
-        <Tweet
-          name='Peter Parker'
-          message='Hi ... it is you friendly spidey.'
-          likes={2314 + " likes"}
-        />
-        <Tweet
-          name='Kang'
-          message='I am Kang, The Conquer.'
-          likes={1331 + " likes"}
-        />
+        {users.map(user => (
+          <Tweet name={user.name} message={user.message} likes={user.likes} />
+        ))}
       </div>
     </div>
   );
