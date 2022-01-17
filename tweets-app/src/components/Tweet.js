011f0210aa903ea65a5342ee6e1538a5e46f0e2e
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-17 13:28:12 +0100

    Create new component Tweet and use Tweet component in App component

---
+++ tweets-app/src/components/Tweet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tweet() {
+  return (
+    <div>
+      <h3>Name</h3>
+      <p>Random tweet message</p>
+      <h3>Number of likes</h3>
+    </div>
+  );
+}
+
+export default Tweet;
