6a1341b3f6ed9a7b8ebe1508e430d8acd9f4c361
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-27 17:55:19 +0100

    Create routing with routes and link properties

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Shop from "./pages/Shop";
 import Nav from "./pages/Nav";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className='App'>
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/shop' element={<Shop />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- react-router/src/pages/Nav.js
+++ react-router/src/pages/Nav.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const navLinksStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+
   return (
     <nav className='nav'>
-      <h2>Logo</h2>
+      <Link style={navLinksStyle} to='/'>
+        <h2>Logo</h2>
+      </Link>
+
       <ul className='nav-links'>
-        <li>About</li>
-        <li>Shop</li>
+        <Link style={navLinksStyle} to='/about'>
+          <li>About</li>
+        </Link>
+
+        <Link style={navLinksStyle} to='/shop'>
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
