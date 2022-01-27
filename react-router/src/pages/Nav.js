d26b4dd4566d89b958bf0bb1e2330d42941452d2
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-27 12:41:27 +0100

    Create pages components and import to App component

---
+++ react-router/src/pages/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function Nav() {
+  return (
+    <nav>
+      <h2>Logo</h2>
+      <ul>
+        <li>About</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
