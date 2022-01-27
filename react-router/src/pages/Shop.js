2307b135df4af361b93f11aca1b0c9a3e227e912
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-27 19:33:17 +0100

    Add useState, useEffect and fetch data from API to shop items'

--- react-router/src/pages/Shop.js
+++ react-router/src/pages/Shop.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "../App.css";
+import { useState, useEffect } from "react";
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch("https://fortnite-api.com/v2/cosmetics/br/new");
+
+    const items = await data.json();
+    console.log(items);
+  };
   return (
     <div>
       <h1>Shop Page</h1>
