8816afb95d41b7ae2b81e786c0e420d9124e395f
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-29 13:37:16 +0100

    Use context in Nav component

--- react-state-management/src/components/Nav.js
+++ react-state-management/src/components/Nav.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useContext } from "react";
+import { MovieContext } from "./MovieContext";
 
 function Nav() {
+  const [movies, setMovies] = useContext(MovieContext);
   return (
     <div className='nav-bar'>
       <h3> Szymon Sz</h3>
-      <p>List of Movies: </p>
+      <p>List of Movies: {movies.length}</p>
     </div>
   );
 }
