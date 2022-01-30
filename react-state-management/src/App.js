ce0a36370b95a4b15677e429f2745fcca04fab10
Szymon Szaminko <sz.szaminko@gmail.com>
2022-01-30 01:59:54 +0100

    Create new component AddMovie and create new movies in the app and add to list

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import Nav from "./components/Nav";
 import { MovieProvider } from "./components/MovieContext";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   return (
     <MovieProvider>
       <div className='App'>
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
