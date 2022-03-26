f45ca9beab86dfce25fa5875b4bfc16f7e6cc14a
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-26 01:42:22 +0100

    Add example about useState

--- react-hooks/src/hooks/UseState/UseStateHook.js
+++ react-hooks/src/hooks/UseState/UseStateHook.js
@@ -3,22 +3,33 @@ import React, { useState } from "react";
 function UseStateHook() {
   const [counter, setCounter] = useState(0);
 
+  const addCounter = newValue => {
+    newValue = counter + 1;
+    setCounter(newValue);
+  };
+
+  const subCounter = newValue => {
+    newValue = counter - 1;
+    setCounter(newValue);
+  };
+
   return (
     <div className='content'>
       <div className='description'>
         <h1>useState Hook</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut debitis
-          animi laboriosam ullam doloribus commodi esse cupiditate asperiores,
-          adipisci molestias, vel dignissimos magnam laborum earum, impedit
-          sequi distinctio maiores quod.
+          Returns a stateful value, and a function to update it. During the
+          initial render, the returned state (state) is the same as the value
+          passed as the first argument (initialState). The setState function is
+          used to update the state. It accepts a new state value and enqueues a
+          re-render of the component.
         </p>
       </div>
 
       <div className='example-app'>
         <div>Counter: {counter}</div>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={subCounter}>-</button>
+        <button onClick={addCounter}>+</button>
       </div>
     </div>
   );
